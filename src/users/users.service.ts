c5a1560e09da9ba76daef014f44828e5d27e9f1e
Hamza <hamzaahmed00@hotmail.com>
2023-09-06 19:34:20 +0500

    create service

---
+++ src/users/users.service.ts
@@ -0,0 +1,43 @@
+import { Injectable, HttpStatus } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { UsersEntity } from './users.entity';
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+  password: string;
+}
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(UsersEntity)
+    private usersRepository: Repository<UsersEntity>,
+  ) {}
+
+  async create(data: User) {
+    const user = this.usersRepository.create(data);
+    await this.usersRepository.save(data);
+    return user;
+  }
+
+  async showAll() {
+    return await this.usersRepository.find();
+  }
+
+  async show(id: number) {
+    return await this.usersRepository.findOne({ where: { id: id } });
+  }
+
+  async update(id: number, data: Partial<User>) {
+    return await this.usersRepository.update({ id }, data);
+  }
+
+  async delete(id: number) {
+    await this.usersRepository.delete({ id });
+    return { deleted: true };
+  }
+}
