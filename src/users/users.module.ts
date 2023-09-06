1c430ed6e255a22701be17061331e5b6eea19fa8
Hamza <hamzaahmed00@hotmail.com>
2023-09-06 19:34:59 +0500

    add users service to module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersEntity } from './users.entity';
+import { UsersService } from './users.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UsersEntity])],
+  providers: [UsersService],
 })
 export class UsersModule {}
